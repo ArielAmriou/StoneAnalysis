c0816b374b3c5e192f7d9d57faba763b691a600a
Pierrick <pierrick.simon@epitech.eu>
2026-05-26 16:51:50 +0200

    feat: analyse mode complete

--- src/StoneAnalysis.cpp
+++ src/StoneAnalysis.cpp
@@ -8,6 +8,8 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
+#include <map>
+#include <iomanip>
 #include "StoneAnalysis.hpp"
 #include "Exception.hpp"
 #include "Wav.hpp"
@@ -110,11 +112,21 @@ namespace StoneAnalysis {
     {
         if (!_in)
             throw NotInitializeException();
-        std::cout << *_in << std::endl;
         auto a = _in->analize();
-        auto size = a.size();
-        int i = 440 * size / 48000;
-        std::cout << a[i].imag() << " : " <<  a[i].real() << std::endl;
+        std::map<double, std::size_t> dists;
+        for (std::size_t i = 0; i < a.size(); i++) {
+            auto dist = std::sqrt(std::pow(a[i].imag(), 2.0) + std::pow(a[i].real(), 2.0));
+            dists.insert({dist, i});
+        }
+        std::cout << "Top " << _n << " frequencies:\n" << std::fixed << std::setprecision(1);
+        auto iter = --dists.end();
+        for (std::size_t i = 0; i < _n; i++) {
+            auto value = iter->second * 48000.0 / a.size();
+            std::cout << value << " Hz\n";
+            if (iter == dists.begin())
+                break;
+            iter--;
+        }
     }
 
     void StoneAnalysis::cypher()
